8dc108fffc82f838b19cef279cf892122b7fe876
Nimm <yandreyev@yahoo.com>
2019-05-27 11:30:57 -0500

    asdf

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { render } from "react-dom";
 import * as serviceWorker from "./serviceWorker";
+import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
+import PageInstance from "./PageInstance";
 
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={PageInstance} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
