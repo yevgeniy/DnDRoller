c61c709c12b1c25ad93d4da361e6b751c7627f6e
Nimm <yandreyev@yahoo.com>
2019-05-29 10:21:37 -0500

    asdf

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { Switch, Route, Link, BrowserRouter, Redirect } from "react-router-dom";
 import PageInstance from "./PageInstance";
+import { withStyles, StyleRules } from "@material-ui/core/styles";
 
 import "./styles.css";
 
@@ -18,7 +19,24 @@ function App() {
   );
 }
 
+const styles = theme => {
+  const styles: StyleRules = {
+    "@global": {
+      // MUI typography elements use REMs, so you can scale the global
+      // font size by setting the font-size on the <html> element.
+      html: {
+        [theme.breakpoints.down("xs")]: {
+          fontSize: 12
+        }
+      }
+    }
+  };
+  return styles;
+};
+
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+//@ts-ignore
+const Comp = withStyles(styles)(App);
+render(<Comp />, rootElement);
 
 serviceWorker.register();
